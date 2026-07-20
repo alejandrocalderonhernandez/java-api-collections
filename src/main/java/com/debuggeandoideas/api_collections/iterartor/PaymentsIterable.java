3b7bb8af12497376ff8a7b9c8c1e806fd2afeb83
Alejandro Calderon <alejandrocalderon@MacBook-Alejandro.local>
2026-07-19 20:03:41 -0600

    add iterator

---
+++ src/main/java/com/debuggeandoideas/api_collections/iterartor/PaymentsIterable.java
@@ -0,0 +1,13 @@
+package com.debuggeandoideas.api_collections.iterartor;
+
+import com.debuggeandoideas.api_collections.dtos.Payment;
+
+import java.util.Iterator;
+
+public class PaymentsIterable implements Iterable<Payment> {
+
+    @Override
+    public Iterator<Payment> iterator() {
+        return null;
+    }
+}
