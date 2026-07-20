9eb502fa1b13943226a5abca536d1c908a2262ab
Alejandro Calderon <alejandrocalderon@MacBook-Alejandro.local>
2026-07-19 20:11:02 -0600

    iterator

--- src/main/java/com/debuggeandoideas/api_collections/iterartor/PaymentsIterable.java
+++ src/main/java/com/debuggeandoideas/api_collections/iterartor/PaymentsIterable.java
@@ -1,13 +1,46 @@
 package com.debuggeandoideas.api_collections.iterartor;
 
+
 import com.debuggeandoideas.api_collections.dtos.Payment;
 
 import java.util.Iterator;
+import java.util.List;
 
-public class PaymentsIterable implements Iterable<Payment> {
+public final class PaymentsIterable implements Iterable<Payment> {
+
+    private final Payment[] payments;
+
+    public PaymentsIterable(List<Payment> payments) {
+        throw new UnsupportedOperationException();
+    }
 
     @Override
     public Iterator<Payment> iterator() {
-        return null;
+        throw new UnsupportedOperationException();
+    }
+
+    private static final class PaymentIterator implements Iterator<Payment> {
+
+        private final Payment[] payments;
+        private int cursor = 0;
+        private Payment nextPayment;
+
+        private PaymentIterator(Payment[] payments) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean hasNext() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Payment next() {
+            throw new UnsupportedOperationException();
+        }
+
+        private void advance() {
+            throw new UnsupportedOperationException();
+        }
     }
 }
---
+++ src/test/java/com/debuggeandoideas/api_collections/iteartor/PaymentsIterableTest.java
@@ -0,0 +1,4 @@
+package com.debuggeandoideas.api_collections.iteartor;
+
+public class PaymentsIterableTest {
+}
