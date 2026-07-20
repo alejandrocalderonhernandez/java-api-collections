95e62c54edeb2943bb41aa38582bece4e481626f
Alejandro Calderon <alejandrocalderon@MacBook-Alejandro.local>
2026-07-20 11:13:21 -0600

    add iterator

--- src/main/java/com/debuggeandoideas/api_collections/iterartor/PaymentsIterable.java
+++ src/main/java/com/debuggeandoideas/api_collections/iterartor/PaymentsIterable.java
@@ -5,18 +5,17 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-
 public final class PaymentsIterable implements Iterable<Payment> {
 
     private final Payment[] payments;
 
-    public PaymentsIterable(List<Payment> paymentsList) {
-        this.payments = paymentsList.toArray(new Payment[0]);
+    public PaymentsIterable(List<Payment> payments) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Iterator<Payment> iterator() {
-        return new PaymentIterator(this.payments);
+        throw new UnsupportedOperationException();
     }
 
     private static final class PaymentIterator implements Iterator<Payment> {
@@ -25,20 +24,17 @@ public final class PaymentsIterable implements Iterable<Payment> {
         private int cursor = 0;
 
         private PaymentIterator(Payment[] payments) {
-            this.payments = payments;
+            throw new UnsupportedOperationException();
         }
 
         @Override
         public boolean hasNext() {
-            return cursor < payments.length;
+            throw new UnsupportedOperationException();
         }
 
         @Override
         public Payment next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-            return payments[cursor++];
+            throw new UnsupportedOperationException();
         }
     }
 }
